39288c380e3ee1950bf1a0a048c31449833e22db
kevinblumluna <kevinblumluna@gmail.com>
2019-06-18 23:16:30 -0500

    Creación clase Cliente

---
+++ src/Usuarios/Cliente.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Usuarios;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Kevin Blum
+ */
+public class Cliente {
+    private String ocupacion;
+    private String ingresosMensuales;
+    private ArrayList solicitudesPendientes;
+    private ArrayList solicitudesRespondidas;
+
+    public Cliente(String ocupacion, String ingresosMensuales, ArrayList solicitudesPendientes, ArrayList solicitudesRespondidas) {
+        this.ocupacion = ocupacion;
+        this.ingresosMensuales = ingresosMensuales;
+        this.solicitudesPendientes = solicitudesPendientes;
+        this.solicitudesRespondidas = solicitudesRespondidas;
+    }
+
+    public String getOcupacion() {
+        return ocupacion;
+    }
+
+    public void setOcupacion(String ocupacion) {
+        this.ocupacion = ocupacion;
+    }
+
+    public String getIngresosMensuales() {
+        return ingresosMensuales;
+    }
+
+    public void setIngresosMensuales(String ingresosMensuales) {
+        this.ingresosMensuales = ingresosMensuales;
+    }
+
+    public ArrayList getSolicitudesPendientes() {
+        return solicitudesPendientes;
+    }
+
+    public void setSolicitudesPendientes(ArrayList solicitudesPendientes) {
+        this.solicitudesPendientes = solicitudesPendientes;
+    }
+
+    public ArrayList getSolicitudesRespondidas() {
+        return solicitudesRespondidas;
+    }
+
+    public void setSolicitudesRespondidas(ArrayList solicitudesRespondidas) {
+        this.solicitudesRespondidas = solicitudesRespondidas;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "ocupacion=" + ocupacion + ", ingresosMensuales=" + ingresosMensuales + ", solicitudesPendientes=" + solicitudesPendientes + ", solicitudesRespondidas=" + solicitudesRespondidas + '}';
+    }
+    
+    
+    
+}
--- src/Usuarios/Usuario.java
+++ src/Usuarios/Usuario.java
@@ -7,8 +7,57 @@ package Usuarios;
 
 /**
  *
- * @author kexbl
+ * @author Kevin Blum
  */
 public class Usuario {
+    private String dni;
+    private String nombre;
+    private String usuario;
+    private String contraseña;
+
+    public Usuario(String dni, String nombre, String usuario) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.usuario = usuario;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "dni=" + dni + ", nombre=" + nombre + ", usuario=" + usuario + ", contrase\u00f1a=" + contraseña + '}';
+    }
     
-}
+    
+    
+ }
